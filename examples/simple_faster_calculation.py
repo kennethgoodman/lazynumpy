071dd1fba3be7438ac103e1043146ee50a18b27b
kennethgoodman <kennethsgoodman@yahoo.com>
2019-01-21 16:07:07 -0500

    example file to show speedup

---
+++ examples/simple_faster_calculation.py
@@ -0,0 +1,60 @@
+import time
+
+import numpy as np
+
+from lazynumpy.larray import lndarray
+
+
+def timeit(method):
+    """ https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d """
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
+
+
+@timeit
+def run_traditional_operations(matrices):
+    rtn = matrices[0]
+    for matrix in matrices[1:]:
+        rtn = np.dot(rtn, matrix)
+    return rtn
+
+
+@timeit
+def run_without_lndarray_but_with_knowledge(matrices):
+    return np.dot(matrices[0], np.dot(matrices[1], matrices[2]))
+
+
+@timeit
+def run_with_lndarray(matrices):
+    initial = lndarray(matrices[0])
+    for matrix in matrices[1:]:
+        matrix = lndarray(matrix)
+        initial = initial.ldot(matrix)
+    return initial.eval()
+
+
+def main():
+    np.random.seed(1)
+    dimensions = 10_000
+    A = lndarray(np.random.random((dimensions, 1)))
+    B = lndarray(np.random.random((1, dimensions)))
+    C = lndarray(np.random.random((dimensions, dimensions)))
+
+    logtime_data = {}
+    run_traditional_operations([A, B, C], logtime_data=logtime_data)
+    run_without_lndarray_but_with_knowledge([A, B, C], logtime_data=logtime_data)
+    run_with_lndarray([A, B, C], logtime_data=logtime_data)
+    print(logtime_data)
+
+
+if __name__ == '__main__':
+    main()
